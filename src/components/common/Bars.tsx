bd8c9585eadc7625e3d2956380776d0a60618279
sadra <sadrataghavi1383@gmail.com>
2021-11-28 23:18:58 +0330

    tryin out a new anim for ultra sonic, see if the owner will buy it or what

---
+++ src/components/common/Bars.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import "./sass/bars.scss"
+
+const Bars = () => {
+    return (
+        <div className="bars">
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+        </div>
+    )
+}
+
+export default Bars
