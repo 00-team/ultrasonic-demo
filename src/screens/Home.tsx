d38ff60ee16ee0c4ff86a96b53399c341bc31e81
sadra <sadrataghavi1383@gmail.com>
2021-11-28 23:18:28 +0330

    about us section added to home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 // layouts 
 import HeroSection from '../layouts/HeroSection'
+import AboutUs from '../layouts/AboutUs'
 
 const Home = () => {
     return (
         <>
             <HeroSection />
+            <AboutUs />
         </>
     )
 }
