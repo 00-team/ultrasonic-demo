4c9555fe8667464df022343ddf62f5a038453de5
sadra <sadrataghavi1383@gmail.com>
2021-11-28 17:30:40 +0330

    home screen created

---
+++ src/screens/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// layouts 
+import HeroSection from '../layouts/HeroSection'
+
+const Home = () => {
+    return (
+        <>
+            <HeroSection />
+        </>
+    )
+}
+
+export default Home
