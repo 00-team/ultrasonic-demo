58e3ef5d5e5f2d5ff0577b6b309bc3d0d0b59edc
sadra <sadrataghavi1383@gmail.com>
2021-11-29 18:12:51 +0330

    contact us added to home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 // layouts 
 import HeroSection from '../layouts/HeroSection'
 import AboutUs from '../layouts/AboutUs'
+import ContactUs from '../layouts/ContactUs'
 
 const Home = () => {
     return (
         <>
             <HeroSection />
             <AboutUs />
+            <ContactUs />
         </>
     )
 }
