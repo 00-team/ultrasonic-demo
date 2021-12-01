697cdf6020f8c84df7584b314cd40a9993636bac
sadra <sadrataghavi1383@gmail.com>
2021-12-01 21:58:18 +0330

    maped thro the blog card comp and worked on blogs container

--- src/layouts/Blogs.tsx
+++ src/layouts/Blogs.tsx
@@ -3,16 +3,40 @@ import React from 'react'
 // style
 import './sass/blogs.scss'
 
+// icons
+import { RiArticleFill } from '@react-icons/all-files/ri/RiArticleFill'
+
 // utils
 import { LazyMotionY } from '../components/common/LazyMotion'
+
+// blog card
+import BlogCard from '../components/common/BlogCard'
+
+// btn comp 
+import { ButtonWithArrow } from '../components/common/Button'
 
 const Blogs = () => {
     return (
         <div className='blog'>
             <div className='container'>
                 <LazyMotionY>
-                    <h2 className='title'>بلاگ </h2>
+                    <h2 className='title'>
+                        <RiArticleFill />
+                        مقالات
+                        <RiArticleFill />
+                    </h2>
                 </LazyMotionY>
+                <div className='wrapper'>
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                </div>
+                <div className="see-more">
+                    <ButtonWithArrow>دیدن همه مقالات</ButtonWithArrow>
+                </div>
             </div>
         </div>
     )
