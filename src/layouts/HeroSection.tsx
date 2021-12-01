9b158996357bc5e5462213d8d93e904cf4334a51
sadra <sadrataghavi1383@gmail.com>
2021-12-02 01:15:05 +0330

    finished hero section for now

--- src/layouts/HeroSection.tsx
+++ src/layouts/HeroSection.tsx
@@ -4,11 +4,15 @@ import React from 'react'
 // import { ButtonWithBackGround } from '../components/common/Button'
 
 // utils
-// import { LazyMotionY } from '../components/common/LazyMotion'
+import {
+    LazyMotionY,
+    LazyMotionX,
+    LazyMotionXReverse,
+} from '../components/common/LazyMotion'
 
 // video
 // import video from '../../static/vids/demo.webm'
-import img from "../../static/imgs/hero.png"
+import img from '../../static/imgs/hero.png'
 
 // style
 import './sass/herosection.scss'
@@ -18,26 +22,24 @@ const HeroSection = () => {
         <div className='hero-container'>
             <div className='content'>
                 <div className='texts'>
-                    <div className='title'>التروسونیک با استاد خسرویان</div>
-                    <h1 className='description'>
-                        ثبت نام <br />
-                        دوره های انلاین <br />و کلاس های حضوری
-                    </h1>
+                    <LazyMotionXReverse>
+                        <div className='title'>التروسونیک با استاد خسرویان</div>
+                    </LazyMotionXReverse>
+                    <LazyMotionX>
+                        <h1 className='description'>
+                            ثبت نام <br />
+                            دوره های انلاین <br />و کلاس های حضوری
+                        </h1>
+                    </LazyMotionX>
                 </div>
-                <div
-                    className='img'
-                    style={{
-                        backgroundImage: `url(${img})`,
-                    }}
-                >
-                    {/* <img
-                        loading='lazy'
-                        src={img}
-                        alt='My Ultrasound Tutor offers One-on-one Live Tutor, Registry Review Courses and Practice Exams'
-                        width='1900'
-                        height='554'
-                    /> */}
-                </div>
+                <LazyMotionY>
+                    <div
+                        className='img'
+                        style={{
+                            backgroundImage: `url(${img})`,
+                        }}
+                    ></div>
+                </LazyMotionY>
             </div>
         </div>
     )
