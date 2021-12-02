4f6113b5ea1d770fe5b3cca08c4781d215e45b1c
sadra <sadrataghavi1383@gmail.com>
2021-12-02 12:01:54 +0330

    clg for new reducer

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+// redux stuff 
+import { useSelector } from 'react-redux'
+import { RootState } from 'src/redux'
 
 // layouts 
 import HeroSection from '../layouts/HeroSection'
@@ -7,6 +11,13 @@ import Blogs from '../layouts/Blogs'
 import ContactUs from '../layouts/ContactUs'
 
 const Home = () => {
+
+    const scollData = useSelector((state:RootState) => state.App.winwid)
+
+    useEffect(() => {
+        console.log(scollData)
+    }, [scollData])
+
     return (
         <>
             <HeroSection />
