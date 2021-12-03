d42ea6139f0570e29b69f025f1cc43bed34c606e
sadra <sadrataghavi1383@gmail.com>
2021-12-03 13:50:10 +0330

    videos container work on and tested

--- src/layouts/Videos.tsx
+++ src/layouts/Videos.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-// icons 
+// icons
 import { AiOutlineFundProjectionScreen } from '@react-icons/all-files/ai/AiOutlineFundProjectionScreen'
 
 // utils
 import { LazyMotionY } from '../components/common/LazyMotion'
 import Slider from '../components/slider/Slider'
-
+import VideoCard from '../components/common/VideoCard'
 
 // style
 import './sass/videos.scss'
@@ -18,28 +18,45 @@ const Videos = () => {
         <div className='videos'>
             <div className='container'>
                 <LazyMotionY>
-                    <h2 className='title'>
+                    <h2 className='title' id="videos">
                         <AiOutlineFundProjectionScreen />
                         دوره ها
                         <AiOutlineFundProjectionScreen />
                     </h2>
                 </LazyMotionY>
-                <h4 className='title'>اخرین دوره های رایگان</h4>
+                <h4 className='title'>اخرین دوره های انلاین</h4>
+                <div className='wrapper'>
+                    <Slider>
+                        {BaseData.map(index => (
+                            <VideoCard
+                                title='عنوان '
+                                description='  بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها '
+                                key={index}
+                            />
+                        ))}
+                    </Slider>
+                </div>
+                <h4 className='title'>اخرین دوره های حضوری</h4>
+                <div className='wrapper'>
+                    <Slider>
+                        {BaseData.map(index => (
+                            <VideoCard
+                                title='عنوان '
+                                description='  بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها '
+                                key={index}
+                            />
+                        ))}
+                    </Slider>
+                </div>
+
+                {/* <h4 className='title'>اخرین دوره های پولی</h4>
                 <div className='wrapper'>
                     <Slider>
                         {BaseData.map((item, index) => (
                             <div key={index}>{item}</div>
                         ))}
                     </Slider>
-                </div>
-                <h4 className='title'>اخرین دوره های پولی</h4>
-                <div className='wrapper'>
-                    <Slider>
-                        {BaseData.map((item, index) => (
-                            <div key={index}>{item}</div>
-                        ))}
-                    </Slider>
-                </div>
+                </div> */}
             </div>
         </div>
     )
